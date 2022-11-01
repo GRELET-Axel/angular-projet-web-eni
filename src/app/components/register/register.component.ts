4216285a848b1edd3e83f6e816acb47ad8e43ca8
FERRE24Mo <72926145+FERRE24Mo@users.noreply.github.com>
2022-11-01 14:32:43 +0100

    suite

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Campus } from 'src/app/models/Campus';
+import { AuthService } from 'src/_services/auth/auth.service';
+import { CampusService } from 'src/_services/campus/campus.service';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +11,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  form: any = {
+    email: null,
+    password: null,
+    nom: null,
+    prenom: null,
+    telephone: null,
+    administrateur: null,
+    campus: null,
+  };
+  campuses: Campus[] | undefined;
+  isSuccessful = false;
+  isSignUpFailed = false;
+  errorMessage = '';
+
+  constructor(private router: Router,private campusService: CampusService,private authService: AuthService) { }
 
   ngOnInit(): void {
+    this.campusService.getCampuss().subscribe(
+      data => {
+        this.campuses = data;
+        },
+      err => {
+        this.errorMessage = err.error.message;
+        this.isSignUpFailed = true;
+      }
+    );
   }
 
+  onSubmit(): void {
+    const { email, password, nom, prenom, telephone, administrateur, campus } = this.form;
+    this.authService.register(email, password, nom, prenom, telephone, administrateur, campus).subscribe(
+      data => {
+        console.log(data);
+        this.isSuccessful = true;
+        this.isSignUpFailed = false;
+      },
+      err => {
+        this.errorMessage = err.error.message;
+        this.isSignUpFailed = true;
+      }
+    );
+
+    this.router.navigate(['/']);
+
+  }
 }
